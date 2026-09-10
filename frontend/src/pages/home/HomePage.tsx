8790c8347dfbfc953c86ac9cde1ba1240afd85a9
Ngọc Vân <nguyenngocvan.qng@gmail.com>
2026-09-10 08:15:23 +0700

    UI: Clean up unused useState import in HomePage

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 import { IoMoonSharp } from "react-icons/io5";
